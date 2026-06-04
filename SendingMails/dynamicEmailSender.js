179b2feb89597429ae0d8cd00d0b2de68bdc74b4
Akshat-1407 <akshat.rastogi1407@gmail.com>
2026-06-04 19:50:44 +0530

    ...

--- SendingMails/dynamicEmailSender.js
+++ SendingMails/dynamicEmailSender.js
@@ -6,17 +6,27 @@ require('dotenv').config();
 async function sendWelcomeEmail(userEmail, userName) {
     try {
 
-        const sendGridDetails = {
-            host: "smtp.sendgrid.net",
-            port: 465,
-            secure: true,
+        // const sendGridDetails = {
+        //     host: "smtp.sendgrid.net",
+        //     port: 465,
+        //     secure: true,
+        //     auth: {
+        //         user: "apikey",
+        //         pass: process.env.SENDGRID_API_KEY
+        //     }
+        // }
+
+        const brevoDetails = {
+            host: "smtp-relay.brevo.com",
+            port: 587,
+            secure: false,
             auth: {
-                user: "apikey",
-                pass: process.env.SENDGRID_API_KEY
-            }
+                user: process.env.BREVO_EMAIL,
+                pass: process.env.BREVO_SMTP_KEY,
+            },
         }
 
-        const transporter = nodemailer.createTransport(sendGridDetails);
+        const transporter = nodemailer.createTransport(brevoDetails);
 
         // 2. Render the EJS template
         // We pass the data { name: userName, email: userEmail } to the template
@@ -25,18 +35,19 @@ async function sendWelcomeEmail(userEmail, userName) {
         // 3. Define Email Options
         const mail = {
             to: userEmail,
-            from: 'code.developer2099@gmail.com',
+            from: process.env.SENDER_EMAIL,
             subject: 'Welcome to the Team!',
             html: data // This is the rendered HTML from EJS
         };
 
         // 4. Send the mail
         const info = await transporter.sendMail(mail);
-        console.log('Email sent successfully:', info.messageId);
+        console.log('Email sent successfully:', info);
 
     } catch (error) {
         console.error('Error sending email:', error);
     }
 }
 
-sendWelcomeEmail('geroma8385@izkat.com', 'John Doe');
+sendWelcomeEmail('citococ683@4nly.com', 'John Doe');
+
